8566a3e2026a752ec815ab1b65e4d3b2630e8006
Code-with-R <raj982269@gmail.com>
2026-07-24 00:08:40 +0530

    create a middleware and a function to possible error

--- MERN_E_state_project/api/index.js
+++ MERN_E_state_project/api/index.js
@@ -5,9 +5,9 @@ import userRouter from './routes/user_route.js';
 import authRouter from './routes/auth_route.js'
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(() =>{
-    console.log('Connected to MongoDB!');  
-}).catch((err)=>{
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Connected to MongoDB!');
+}).catch((err) => {
     console.log(err);
 });
 
@@ -15,9 +15,19 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log('Server is running on port 5000!');
 });
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
