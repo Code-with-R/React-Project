010ba7a73e7fa9b17102fa04344d15e35b106543
Code-with-R <raj982269@gmail.com>
2026-07-25 13:40:18 +0530

    update the header and make the profile page is private.

--- MERN_E_state_project/client/src/App.jsx
+++ MERN_E_state_project/client/src/App.jsx
@@ -6,17 +6,20 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import Header from './Components/Header';
+import PrivateRoute from './Components/privateRoute';
 
 
 export default function App() {
   return <BrowserRouter>
-  <Header />
+    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/sign-up' element={<SignUp />} />
-      <Route path='/profile' element={<Profile />} />
       <Route path='/about' element={<About />} />
+      <Route element={<PrivateRoute/>}>
+      <Route path='/profile' element={<Profile />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 }
