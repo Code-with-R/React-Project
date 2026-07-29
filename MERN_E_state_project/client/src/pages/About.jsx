8314f04830ce8a713c57d53532175169950ec891
Code-with-R <raj982269@gmail.com>
2026-07-29 16:26:16 +0530

    complete about page

--- MERN_E_state_project/client/src/pages/About.jsx
+++ MERN_E_state_project/client/src/pages/About.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-700'> 
+        About Raj Estate
+      </h1>
+      <p className='mb-4 text-slate-700 font-bold'>
+        Welcome to Raj Estate – Your Trusted Real Estate Partner
+      </p>
+      <p className='mb-4 text-slate-700'>At Raj Estate, we make finding your dream property simple, fast, and secure. Whether you're looking to buy, sell, or rent a home, apartment, villa, or commercial property, our platform offers a wide range of verified listings to suit every lifestyle and budget.</p>
+      <p className='mb-4 text-slate-700'>Our mission is to connect buyers, sellers, landlords, and tenants through a modern, user-friendly platform powered by the latest technology. We provide detailed property information, high-quality images, pricing, location details, and essential amenities so you can make informed decisions with confidence.</p>
+      <p className='mb-4 text-slate-700'>With secure user authentication, personalized profiles, advanced property search, and an easy listing management system, Raj Estate provides a seamless experience for both property owners and customers.</p>
+
+    </div>
   )
 }
